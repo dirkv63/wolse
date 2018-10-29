208e6120d0491ec358e388a1209239067455132b
Dirk Vermeylen <dirk.vermeylen@skynet.be>
2018-10-29 22:06:46 +0100

    Layout and bu small changes

--- neo_db_bu.sh
+++ neo_db_bu.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+HOME=/home/dirk/wolse
 source /opt/envs/flrun/bin/activate
-python3 tools/neo_action.py -a stop
-python3 tools/stop_webserver.py
-python3 tools/neo_bu.py
-python3 tools/neo_action.py -a start
+python3 $HOME/tools/neo_action.py -a stop
+python3 $HOME/tools/stop_webserver.py
+python3 $HOME/tools/neo_bu.py
+python3 $HOME/tools/neo_action.py -a start
 sleep 60
-python3 wolse.py &
+python3 $HOME/wolse.py &
