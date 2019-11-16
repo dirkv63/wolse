f61a71eca58560f457d53fb8c611d9595c3df596
Dirk Vermeylen <dirk.vermeylen@skynet.be>
2019-11-16 19:42:10 +0100

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # this script is used to boot a Docker container
-source zolse/bin/activate
+source /opt/envs/olse/bin/activate
 sleep 20
 # flask run
-exec gunicorn -b :5000 --access-logfile - --error-logfile - fromflask:app
+exec gunicorn -b :5000 --access-logfile - --error-logfile - fromflask:app &
