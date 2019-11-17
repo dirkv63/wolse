8b822e0f8628d0380e8978786e9137510e982f2c
Dirk Vermeylen <dirk.vermeylen@skynet.be>
2019-11-17 18:36:16 +0100

    boot shebang

--- boot.sh
+++ boot.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+# be careful which shebang statement to set to make sure source is found...
 # this script is used to boot a Docker container
 source /opt/envs/olse/bin/activate
-sleep 20
+# sleep 20
 # flask run
-exec gunicorn -b :5000 --access-logfile - --error-logfile - fromflask:app &
+exec gunicorn -b :19033 --access-logfile - --error-logfile - fromflask:app &
